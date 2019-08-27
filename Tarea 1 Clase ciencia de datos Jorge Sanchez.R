ff66216c907a972e5b3a79a54d8b7a2428a609a3
JrSanCed <sanchez20890@gmail.com>
2019-08-27 15:29:50 -0500

    Tarea 1

---
+++ Tarea 1 Clase ciencia de datos Jorge Sanchez.R
@@ -0,0 +1,28 @@
+#Ejercicio 1
+s<-0
+for (i in 1:99)
+  if (i%%2==1){
+  s <- s + i
+}
+s
+
+
+#Ejercicio 2
+n = 1
+s =0
+while (n < 100 ){
+    s = s + n
+    n = n + 2
+    }
+
+s
+
+#Ejercicio 3
+x<- c(1:99)
+sum(x[seq(1, length(x), 2)])
+  
+  
+  
+  
+  
+  
